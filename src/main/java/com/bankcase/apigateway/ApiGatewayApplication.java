37817115330e493fce199379501b154b31618487
ahmetbkucuk <ahmetbkucuk@gmail.com>
2025-11-13 02:46:10 +0300

    fix package name for ApiGatewayApplication to align with naming conventions

--- src/main/java/com/bankcase/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/bankcase/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.bankcase.api_gateway;
+package com.bankcase.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

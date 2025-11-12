e11d639d1e57501f1616ca4674c7b0b859a5244b
ahmetbkucuk <ahmetbkucuk@gmail.com>
2025-11-13 02:51:16 +0300

    refactor SecurityConfig to enhance security configuration and improve readability

--- src/main/java/com/bankcase/apigateway/config/SecurityConfig.java
+++ src/main/java/com/bankcase/apigateway/config/SecurityConfig.java
@@ -1,24 +1,36 @@
 package com.bankcase.apigateway.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+    public SecurityConfig() {
+        System.out.println("🏗️ SecurityConfig CONSTRUCTOR çağrıldı!");
+    }
+
     @Bean
+    @Order(1)
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/auth/**").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .build();
+            .csrf(csrf -> csrf.disable())
+            .httpBasic(basic -> basic.disable())
+            .formLogin(form -> form.disable())
+            .authorizeExchange(exchange ->
+                    exchange.pathMatchers("/auth/**").permitAll()
+                            .anyExchange().authenticated())
+            .build();
     }
 }
 
